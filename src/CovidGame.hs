b70938d5973c28cc110757238dbce4087bb0ecc4
Harry Askham <harryaskham@gmail.com>
2020-04-13 20:11:59 +0100

    An example of a non-terrible description watcher

--- src/CovidGame.hs
+++ src/CovidGame.hs
@@ -10,7 +10,11 @@ import qualified Data.Map.Strict as M
 import Control.Lens
 
 bedroomDesc :: Description
-bedroomDesc st eID = "this is bedroom"
+bedroomDesc st eID = evalState go st
+  where
+    go = do
+      e <- getEntity eID
+      return $ e^.?name
 
 buildCovidGame :: (MonadState GameState m) => m ()
 buildCovidGame = do
