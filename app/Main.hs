0f36ee239acd2831ba187461ccafb8f8b1058377
Harry Askham <harryaskham@gmail.com>
2021-12-27 16:42:34 +0000

    Separate binaries per game

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,4 @@
 module Main where
 
-import qualified HIF.Games.ExampleGame as EG
-import qualified HIF.Games.FourthGame as FG
-import HIF.Runner
-
 main :: IO ()
---main = runWithBuilder EG.buildGame
-main = runWithBuilder FG.buildGame
+main = putStrLn "Hello, World!"
