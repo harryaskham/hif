a6890029e521fb731158d2ca1683c5a28d3adfc8
Harry Askham <harryaskham@gmail.com>
2021-12-27 15:50:43 +0000

    FG

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import HIF.Runner
 import qualified HIF.Games.ExampleGame as EG
+import qualified HIF.Games.FourthGame as FG
+import HIF.Runner
 
 main :: IO ()
-main = runWithBuilder EG.buildGame
+--main = runWithBuilder EG.buildGame
+main = runWithBuilder FG.buildGame
