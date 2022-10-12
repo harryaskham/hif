43dc1d82325132e0a7ccb511438db23cf8fc3a5b
Harry Askham <harryaskham@gmail.com>
2022-10-12 11:04:42 +0100

    begins session mgmt

---
+++ src/HIF/Session.hs
@@ -0,0 +1,28 @@
+{- TODO: need to fix stack or convert to cabal and add this to the project proper -}
+{-# LANGUAGE OverloadedStrings #-}
+
+module HIF.Session where
+
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text
+import qualified Data.Text as T
+import HIF.Engine
+import HIF.GameState
+import HIF.Instruction
+import HIF.Runner
+
+newtype SessionId = SessionId Text
+
+data Session = Session
+  { _sessionId :: SessionId,
+    _app :: App (),
+    _gameState :: GameState
+  }
+
+mkSession :: App () -> IO Session
+mkSession builder = Session <$> mkSessionId <*> pure (runApp builder) <*> pure mkGameState
+
+newtype SessionRunner = SessionRunner
+  { _sessions :: Map SessionId Session
+  }
